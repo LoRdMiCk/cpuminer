6d3f6d0beb9322f6c9017be0c2fc1d52329becf6
LoRdMiCk <LoRdMiCk@users.noreply.github.com>
2021-01-31 01:51:25 -0500

    Update miner-cpuchain.sh

--- miner-cpuchain.sh
+++ miner-cpuchain.sh
@@ -5,7 +5,7 @@
 #       stratum-eu.rplant.xyz   /London/
 #       stratum-asia.rplant.xyz /Singapore/
 #
-CPU=$((#(nproc)-1))
+CPU=$(($(nproc)-1))
 
 while [ 1 ]; do
 ./cpuminer-aes -a cpupower -o stratum+tcp://mining.cryptorrency.com:3032 -u CVNgkb2BsXAKSUU188oqFRG6ibgLMrKCEi.PXE -t $CPU
